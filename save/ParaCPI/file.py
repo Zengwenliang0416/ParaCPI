fb965f7e991fbc55a32555b924094d58561557d0
Zengwenliang0416 <wenliang_zeng416@163.com>
2024-02-20 12:26:24 +0800

    ParaCPI的对比实验的实验结果和CPIDSCNN在human数据集上的实验结果

---
+++ save/ParaCPI/file.py
@@ -0,0 +1,51 @@
+import glob
+import os
+import shutil
+
+# 定义搜索的起始目录
+start_dir = '.'  # 当前目录
+
+# 定义搜索模式
+pattern = '*_celegans/raw/*/fold_*/log/train/Train.log'
+
+# 使用glob找到所有匹配的文件
+files = glob.glob(os.path.join(start_dir, pattern), recursive=True)
+
+# 循环处理每个找到的文件
+for file_path in files:
+    # 提取fold部分
+    fold_part = file_path.split(os.sep)[4]  # 根据目录结构调整索引
+    # 定义新路径
+    new_path = os.path.join('celegans', file_path.split(os.sep)[3], fold_part, 'log', 'train', 'Train.log')
+    # 创建新目录
+    os.makedirs(os.path.dirname(new_path), exist_ok=True)
+    # 移动文件
+    shutil.copy(file_path, new_path)
+    print(f"复制文件：{file_path} 到 {new_path}")
+
+print("所有文件复制完成。")
+
+
+
+# 定义搜索的起始目录
+start_dir = '.'  # 当前目录
+
+# 定义搜索模式
+pattern = '*_human/raw/*/fold_*/log/train/Train.log'
+
+# 使用glob找到所有匹配的文件
+files = glob.glob(os.path.join(start_dir, pattern), recursive=True)
+
+# 循环处理每个找到的文件
+for file_path in files:
+    # 提取fold部分
+    fold_part = file_path.split(os.sep)[4]  # 根据目录结构调整索引
+    # 定义新路径
+    new_path = os.path.join('human', file_path.split(os.sep)[3], fold_part, 'log', 'train', 'Train.log')
+    # 创建新目录
+    os.makedirs(os.path.dirname(new_path), exist_ok=True)
+    # 移动文件
+    shutil.copy(file_path, new_path)
+    print(f"复制文件：{file_path} 到 {new_path}")
+
+print("所有文件复制完成。")
